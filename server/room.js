580e162e3a80e761ef64bfe8187253f90522acc4
Yarnemeuris <yarne.meuris@gmail.com>
2025-08-06 01:05:58 +0200

    fixed wrong guess bug

--- server/room.js
+++ server/room.js
@@ -11,50 +11,52 @@ module.exports = class room {
 
         this.io = io;
 
-        this.killFunction = () => {}
+        this.killFunction = () => { }
     }
 
     addPlayer(socket, card) {
         if (Object.keys(this.players).length >= 2) return 0
 
-        
         socket.join(this.roomName);
-        
+
         socket.on("disconnect", () => {
             this.io.to(this.roomName).emit("gameEnd", { "error": "disconnect" });
             this.killFunction();
         });
-        
+
         socket.emit("waitForPlayer");
-        
+
         this.players[this.players[colors[0]] == undefined ? colors[0] : colors[1]] = { "card": card, "socket": socket }
-        
+
         if (Object.keys(this.players).length === 2) this.#startGame();
     }
-    
+
     #startGame() {
         for (var color in this.players) {
             var player = this.players[color]
             player.socket.emit("startgame", { "color": color, "opponentColor": oppositeColor[color] })
         }
-        
+
         this.#startTurn();
     }
-    
+
     #endTurn() {
         this.turn = oppositeColor[this.turn];
 
         this.#startTurn();
     }
-    
+
     #startTurn() {
         this.io.to(this.roomName).emit("startTurn", { "turn": this.turn });
-        
+
         this.players[this.turn].socket.once('guessCard', (card) => {
             this.players[this.turn].socket.removeAllListeners("question");
-            
-            if (card != this.players[oppositeColor[this.turn]].card) this.#endTurn;
-            
+
+            if (card != this.players[oppositeColor[this.turn]].card) {
+                this.#endTurn();
+                return;
+            }
+
             this.io.to(this.roomName).emit("gameEnd", { "won": this.turn, "greenCard": this.players.green.card, "redCard": this.players.red.card });
             this.killFunction();
         });
